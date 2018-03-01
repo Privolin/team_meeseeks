3d0a52131a9bcfecf71ab0fdfb2a74a8db341928
Privolin Naidoo <privolin.naidoo@entelect.co.za>
2018-03-01 22:15:46 +0200

    new file naive implem

--- naive_implementation.py
+++ naive_implementation.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Thu Mar  1 22:14:14 2018
+
+@author: privolin.naidoo
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Mar  1 17:48:21 2018
 
 @author: privolin.naidoo
